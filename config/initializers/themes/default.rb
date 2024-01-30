96164281cfc07b54953362a888fd291d8fd5e6da
mediocrehacker <mediocrehacker@protonmail.com>
2024-01-30 22:24:11 +0100

    wip

--- config/initializers/themes/default.rb
+++ config/initializers/themes/default.rb
@@ -5,8 +5,8 @@
 
 Spina::Theme.register do |theme|
   # All views are namespaced based on the theme's name
-  theme.name = "default"
-  theme.title = "Тема по умолчанию"
+  theme.name = 'default'
+  theme.title = 'Тема по умолчанию'
 
   # Parts
   # Define all editable parts you want to use in your view templates
@@ -20,37 +20,59 @@ Spina::Theme.register do |theme|
   # - Attachment
   # - Option
   # - Repeater
-  theme.parts = [
-    {name: "text", title: "Body", hint: "Your main content", part_type: "Spina::Parts::Text"}
-  ]
+  theme.parts = [{
+    name: 'content',
+    title: 'Content',
+    part_type: 'Spina::Parts::Text'
+  }, {
+    name: 'portfolio', # added this part
+    title: 'Portfolio',
+    part_type: 'Spina::Parts::Text'
+  }, {
+    name: 'header_image',
+    title: 'Header image',
+    part_type: 'Spina::Parts::Image'
+  }, {
+    name: 'header',
+    title: 'header',
+    part_type: 'Spina::Parts::Text'
+  }]
 
   # View templates
   # Every page has a view template stored in app/views/my_theme/pages/*
   # You define which parts you want to enable for every view template
   # by referencing them from the theme.parts configuration above.
-  theme.view_templates = [
-    {name: "homepage", title: "Homepage", parts: %w[text]},
-    {name: "show", title: "Page", parts: %w[text]}
-  ]
+  theme.view_templates = [{
+    name: 'homepage',
+    title: 'Homepage',
+    parts: %w[content header_image]
+  }, {
+    name: 'show',
+    title: 'Default',
+    description: 'A simple page',
+    usage: 'Use for your content',
+    parts: %w[content portfolio] # added 'portfolio'
+  }]
 
   # Custom pages
   # Some pages should not be created by the user, but generated automatically.
   # By naming them you can reference them in your code.
   theme.custom_pages = [
-    {name: "homepage", title: "Homepage", deletable: false, view_template: "homepage"}
+    { name: 'homepage', title: 'Главная', deletable: false, view_template: 'homepage' }
   ]
 
   # Navigations (optional)
   # If your project has multiple navigations, it can be useful to configure multiple
   # navigations.
   theme.navigations = [
-    {name: "main", label: "Main navigation"}
+    { name: 'main', label: 'Главное Меню' },
+    { name: 'footer', label: 'Меню Футера' }
   ]
 
   # Layout parts (optional)
   # You can create global content that doesn't belong to one specific page. We call these layout parts.
   # You only have to reference the name of the parts you want to have here.
-  theme.layout_parts = []
+  theme.layout_parts = %w(header)
 
   # Resources (optional)
   # Think of resources as a collection of pages. They are managed separately in Spina
